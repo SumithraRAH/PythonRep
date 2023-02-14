a55e90b99e0bf2ac20d3ead4dd2a2c7a21a37f8d
SumithraH <Sumithra.hormese@gmail.com>
2023-02-14 13:08:05 +0530

    chg

--- classes.py
+++ classes.py
@@ -8,15 +8,16 @@ class Motorcycle:
         self.make = make
         self.model =model
 
-    def turn_engine_on(self):
-        self.is_engine_on = True
-        self.is_headlight_on = True
-
+    
     def __repr__(self):
         return(f'{self.make} {self.model} with engine'
-               f'{"on" if self.is_engine_on else "off"}'
-               f' and headlight {"on" if self.is_headlight_on else "off"}')    
-
+               f'{"on" if self.is_engine_on else "off"} and headlight '
+               f'{"on" if self.is_headlight_on else "off"}')    
+    
+    def turn_engine_on(self):
+        self.is_engine_on = True
+        
+ 
 moto = Motorcycle('Triumph' , 'Thruxton')
 print(moto)
 print(moto.is_engine_on)
