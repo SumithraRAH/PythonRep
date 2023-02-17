5d08695bed4b03c4faf9a6684cd39e6762c71797
SumithraH <Sumithra.hormese@gmail.com>
2023-02-17 12:57:00 +0530

    class

--- classes.py
+++ classes.py
@@ -81,19 +81,6 @@ class Car(Vehicle):
             print("Didn't understand that direction")
 
 
- 
-moto = Motorcycle('Triumph', 'Thruxton')
-car = Car('Honda' ,'Civic')
-for vehicle in [moto,car]:
-    print(vehicle)
-    vehicle.turn_engine_on()
-    vehicle.turn_headlight_on()
-    vehicle.start_driving()
-    vehicle.turn('left')
-    vehicle.turn('right')
-    vehicle.stop_driving()
-    vehicle.turn_engine_off()
-    vehicle.turn_headlight_off()
-    print()
+
 
 
